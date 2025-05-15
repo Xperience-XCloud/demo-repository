3a713200ff988ef10ea7336039cb3127ed8f8919
Jonathan P. Peixoto 喬尼·佩肖托 <jonypeixotooriginal@gmail.com>
2025-05-15 14:16:00 -0300

    Add files via upload

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#252a33] text-white py-4 text-center text-sm">
+      <div className="container-custom">
+        <p>Xperience xCloud is not affiliated with Microsoft or Xbox.</p>
+        <p>© {new Date().getFullYear()} Xperience xCloud</p>
+      </div>
+    </footer>
+  )
+}
